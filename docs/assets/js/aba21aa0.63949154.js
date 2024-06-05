468b643d76235b9b2ddf48818990259ba624825f
DecafDev <40307803+decaf-dev@users.noreply.github.com>
2024-06-05 13:54:10 -0600

    refactor: rename to docs

--- docs/assets/js/aba21aa0.63949154.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkvaultexplorer_com=self.webpackChunkvaultexplorer_com||[]).push([[5742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
