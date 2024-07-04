6fa6a5dd3a11c688aa9888b54db702a151e562eb
DecafDev <40307803+decaf-dev@users.noreply.github.com>
2024-07-03 21:13:24 -0600

    chore: add editor config

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 
 interface Props {
-    name: string;
-    width?: string;
-    maxWidth?: string;
+	name: string;
+	width?: string;
+	maxWidth?: string;
 }
 
 const BUNNY_CDN_URL = "https://vaultexplorer.b-cdn.net";
 
-export default function Video({ name, width, maxWidth}: Props): JSX.Element {
-    const src = `${BUNNY_CDN_URL}/${name}`;
+export default function Video({ name, width, maxWidth }: Props): JSX.Element {
+	const src = `${BUNNY_CDN_URL}/${name}`;
 
 	return (
-        <video src={src} controls style={{ width: width ? width : "100%", maxWidth }}>
-        Your browser does not support the video tag.
-        </video>
+		<video
+			src={src}
+			controls
+			style={{ width: width ? width : "100%", maxWidth }}
+		>
+			Your browser does not support the video tag.
+		</video>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,45 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
 // import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import Heading from "@theme/Heading";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/quickstart">
-            Get started
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<header className={clsx("hero hero--primary", styles.heroBanner)}>
+			<div className="container">
+				<Heading as="h1" className="hero__title">
+					{siteConfig.title}
+				</Heading>
+				<p className="hero__subtitle">{siteConfig.tagline}</p>
+				<div className={styles.buttons}>
+					<Link
+						className="button button--secondary button--lg"
+						to="/docs/quickstart"
+					>
+						Get started
+					</Link>
+				</div>
+			</div>
+		</header>
+	);
 }
 
 export default function Home(): JSX.Element {
-  // const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title="Home"
-      description="Obsidian.md plugin that allows you to explore your vault in visual format.">
-      <HomepageHeader />
-      {/* <main>
+	// const {siteConfig} = useDocusaurusContext();
+	return (
+		<Layout
+			title="Home"
+			description="Obsidian.md plugin that allows you to explore your vault in visual format."
+		>
+			<HomepageHeader />
+			{/* <main>
         <HomepageFeatures />
       </main> */}
-    </Layout>
-  );
+		</Layout>
+	);
 }
